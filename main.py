24f19be546d4f29f5cdd442bf3b004762f19495f
Lucas Costa <141447167+lucasr099@users.noreply.github.com>
2025-12-30 22:37:02 -0300

    update com cors

--- main.py
+++ main.py
@@ -2,32 +2,47 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, Request, Response
 
-from fastapi import FastAPI
-from app.auth import router as auth_router
-from app.chat import router as chat_router  # ✅ IMPORTAÇÃO CORRETA
 from app.database import Base, engine
+from app.auth import router as auth_router
+from app.chat import router as chat_router
 
 app = FastAPI(title="BibliaChat API")
 
-
-@app.get("/ping")
-def ping():
-    return {"status": "pong"}
-
-# cria tabelas
-Base.metadata.create_all(bind=engine)
+# CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",  # frontend
+        "https://helpholyia.squareweb.app",
+        "https://iacristao.squareweb.app",
+        "http://localhost:5173",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# rotas
+@app.options("/{path:path}")
+async def options_handler(path: str, request: Request):
+    origin = request.headers.get("origin")
+
+    return Response(
+        status_code=200,
+        headers={
+            "Access-Control-Allow-Origin": origin or "*",
+            "Accessys-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
+            "Access-Control-Allow-Headers": "Authorization,Content-Type",
+            "Access-Control-Allow-Credentials": "true",
+        },
+    )
+
+@app.get("/ping")
+def ping():
+    return {"status": "pong"}
+
+Base.metadata.create_all(bind=engine)
+
 app.include_router(auth_router, prefix="/auth", tags=["Auth"])
 app.include_router(chat_router, tags=["Chat"])
 
